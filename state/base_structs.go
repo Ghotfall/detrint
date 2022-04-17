c953b727996a3e04c66ab574a55fe2f374a54c91
Andrii Myrhorodskyi <mirgorodskijav@gmail.com>
2022-04-17 14:36:35 +0300

    Update connection creation

--- state/base_structs.go
+++ state/base_structs.go
@@ -5,7 +5,6 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"strings"
 )
 
 type State struct {
@@ -22,15 +21,37 @@ func (s Set) Deploy(i inv.Inventory, l *zap.Logger) {
 		hosts := i.ResolveHosts(state.Hosts)
 		l.Info("Resolved host", zap.Any("hosts", hosts))
 
-		for host, machine := range hosts {
-			addr := machine.Address
-			if !strings.Contains(addr, ":") {
-				addr = addr + ":7056" // TODO: separate default value
+		for _, machine := range hosts {
+			// Get correct target address
+			target, targetErr := machine.Target()
+			if targetErr != nil {
+				l.Error(
+					"Can't make target string",
+					zap.String("address", machine.Address),
+					zap.Error(targetErr),
+				)
+				continue
 			}
 
-			conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-			if err != nil {
-				l.Error("Failed to create client connection", zap.String("target", addr), zap.Error(err))
+			// Create gRPC-connection for client creation
+			conn, dialErr := grpc.Dial(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if dialErr != nil {
+				l.Error(
+					"Failed to create client connection, skipping target",
+					zap.String("target", target),
+					zap.Error(dialErr),
+				)
+				continue
+			}
+
+			// Close gRPC-connection
+			closeErr := conn.Close()
+			if closeErr != nil {
+				l.Error(
+					"Failed to close client connection",
+					zap.String("target", target),
+					zap.Error(closeErr),
+				)
 			}
 		}
 	}
