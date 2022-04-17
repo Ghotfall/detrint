2500510b0bd6c68a8eab82bb6ef9d41f80aed94d
Andrii Myrhorodskyi <mirgorodskijav@gmail.com>
2022-04-17 13:53:17 +0300

    Add target determination logic

--- inv/machine.go
+++ inv/machine.go
@@ -1,8 +1,28 @@
 package inv
 
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"strings"
+)
+
 type Machine struct {
 	Address   string                 `toml:"address"`
 	Username  string                 `toml:"username"`
 	Password  string                 `toml:"password"`
 	Variables map[string]interface{} `toml:"variables,omitempty"`
+}
+
+func (m Machine) Target(defaultPort string) (string, error) {
+	if len(strings.TrimSpace(m.Address)) == 0 {
+		return "", fmt.Errorf("address field is empty, can't make target")
+	}
+
+	u := url.URL{Host: m.Address}
+	if len(u.Port()) == 0 {
+		return net.JoinHostPort(u.Hostname(), defaultPort), nil
+	} else {
+		return m.Address, nil
+	}
 }
