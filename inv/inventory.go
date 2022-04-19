5c09a61f36bd6cc7d97e2e3cc431476f15c2503c
Andrii Myrhorodskyi <mirgorodskijav@gmail.com>
2022-04-19 17:59:02 +0300

    Add method to get machine specific vars

--- inv/inventory.go
+++ inv/inventory.go
@@ -1,5 +1,7 @@
 package inv
 
+import "fmt"
+
 type Inventory struct {
 	Machines map[string]Machine `toml:"machines"`
 	Groups   map[string]Group   `toml:"groups"`
@@ -22,4 +24,30 @@ func (i Inventory) ResolveHosts(name string) map[string]Machine {
 	} else {
 		return map[string]Machine{}
 	}
+}
+
+func (i Inventory) GetMachineVars(servername string) (map[string]interface{}, error) {
+	result := make(map[string]interface{})
+
+	machine, ok := i.Machines[servername]
+	if !ok {
+		// return empty map, not null
+		return result, fmt.Errorf("can't find machine with name '%s'", servername)
+	}
+
+	for _, group := range i.Groups {
+		for _, member := range group.Members {
+			if member == servername {
+				for k, v := range group.Variables {
+					result[k] = v
+				}
+			}
+		}
+	}
+
+	for k, v := range machine.Variables {
+		result[k] = v
+	}
+
+	return result, nil
 }
